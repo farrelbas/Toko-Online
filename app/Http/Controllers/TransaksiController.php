ebc1471c2258d5b37f2bfb7d5be8574da1005b49
farrelbuzz067 <farrel_baswara_28rpl@student.smktelkom-mlg.sch.id>
2021-10-09 16:18:58 +0700

    insert transaksi

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Transaksi;
+use Illuminate\Support\Facades\Validator;
 
 class TransaksiController extends Controller
 {
-    //
+    public function insert_admin(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'tanggal_transaksi' => 'required',
+            'id_pelanggan' => 'required',
+            'password_admin' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $data['status'] = false;
+            $data['message'] = $validator->errors();
+            return Response()->json($data);
+        }
+        $simpan = Transaksi::create([
+            'tanggal_transaksi' => $request->tanggal_transaksi,
+            'id_pelanggan' => $request->id_pelanggan,
+            'status' => $request->status,
+        ]);
+        if ($simpan) {
+            $data['status'] = true;
+            $data['message'] = "Sukses menambahkan data";
+        } else {
+            $data['status'] = false;
+            $data['message'] = "Gagal menambahkan data";
+        }
+        return Response()->json($data);
+    }
 }
